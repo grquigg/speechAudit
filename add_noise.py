22f413f1d036bbd4eda68fe7d9e4777f48df4132
Greg Quigg <gquigg@umass.edu>
2022-11-14 17:56:52 -0500

    Add more documentation

--- add_noise.py
+++ add_noise.py
@@ -6,11 +6,13 @@ import matplotlib.pyplot as plt
 # Load the example clip
 
 def add_noise(audio, method='gaussian', mu=0, sigma=8):
+    #convert audio into float for adding noise
     audio = audio.astype(float)
     X = librosa.stft(audio)
     gaussian = np.random.normal(loc=mu, scale=sigma, size=(X.shape))
     X += gaussian
-    # #convert audio back
+    # #convert audio back into signal
     Xn = librosa.istft(X)
+    #return the noisy audio signal as a short int for deepspeech
     return Xn.astype(np.short)
 
