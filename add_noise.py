d22c6525ee8825013cc17b6f2c52296098acfba5
Shreeviknesh Sankaran <shreeviknesh@gmail.com>
2022-11-24 13:10:02 -0500

    Added pre-loading method in superimpose noise

--- add_noise.py
+++ add_noise.py
@@ -4,6 +4,8 @@ import random
 
 import numpy as np
 import librosa
+import wave
+
 import audiomentations
 from audiomentations import AddGaussianSNR, TimeStretch, PitchShift, Shift
 import albumentations as Alb
@@ -48,35 +50,52 @@ def minimum_suppression(X, params):
     X = np.multiply(X, mask)
     return X
 
+# Helper class to pre-load the noise stfts
+class SuperImposeNoise:
+    def __init__(self):
+        self.mag = 0
+        self.noi = []
+        self.__loaded = False
+
+    def is_loaded(self):
+        return self.__loaded
+
+    def load_noise(self, noise_dir, noise_mag):
+        for nf in os.listdir(noise_dir):
+            fin = wave.open(os.path.join(noise_dir, nf))
+            audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
+            noise = librosa.stft(audio.astype(float))
+            self.noi.append(noise)
+        self.mag = noise_mag
+        self.__loaded = True
+    
+    def add_noise(self, X):
+        for noise in self.noi:
+            assert(X.shape[0] == noise.shape[0]) # n_fft should be same
+            if noise.shape[1] > X.shape[1]:
+                start_idx = np.random.randint(0, noise.shape[1] - X.shape[1] + 1)
+                noise_sam = noise[:, start_idx:start_idx + X.shape[1]]
+            else:
+                num_stack = 1 + X.shape[1] // noise.shape[1]
+                noise_sam = np.tile(noise, num_stack)[:, :X.shape[1]]
+            X += noise_sam * self.mag
+        return X
+
+SINObj = SuperImposeNoise()
 def superimpose(X, params):
     # TODO
-    # - (IMPORTANT) come up with a way to pre-load or store the noise
-    # instead of reading it every time for every input !!
     # - get sample_rate from client.py to make sure both audio & noise
     # files are sampled in the same sample_rate
     # - add multiple noise_src files & change noise_mag to be random weights
     # to have weighted noise added to X (maybe even make sure that the sum
     # of all the random weights equal to a max_mag)
 
-    no_dir = params['noise_dir'] if 'noise_dir' in params else 'noise_src'
+    noise_dir = params['noise_dir'] if 'noise_dir' in params else 'noise_src'
     noise_mag = params['noise_mag'] if 'noise_mag' in params else 1
-    
-    for no_file in os.listdir(no_dir):
-        fin = wave.open(os.path.join(no_dir, no_file))
-        audio = np.frombuffer(fin.readframes(fin.getnframes()), np.int16)
-        noise = librosa.stft(audio.astype(float))
-        assert(X.shape[0] == noise.shape[0]) # n_fft should be same
 
-        if noise.shape[1] > X.shape[1]:
-            start_idx = np.random.randint(0, noise.shape[1] - X.shape[1] + 1)
-            noise_sam = noise[:, start_idx:start_idx + X.shape[1]]
-        else:
-            num_stack = 1 + X.shape[1] // noise.shape[1]
-            noise_sam = np.tile(noise, num_stack)[:, :X.shape[1]]
-        
-        X += noise_sam * noise_mag
-
-    return X
+    if not SINObj.is_loaded():
+        SINObj.load_noise(noise_dir, noise_mag)
+    return SINObj.add_noise(X)
 
 def lengthen(X):
     X_long = np.zeros((X.shape[0], X.shape[1]*2), dtype=complex)
