0147a2ce6d6721ad5adfc6c92d7a5f5e8cdbf7a6
Greg Quigg <gquigg@umass.edu>
2022-12-02 09:07:53 -0800

    Fix error in real_world_noise.py

--- add_noise.py
+++ add_noise.py
@@ -5,7 +5,6 @@ import random
 import numpy as np
 import librosa
 import wave
-
 import audiomentations
 from audiomentations import AddGaussianSNR, TimeStretch, PitchShift, Shift
 from PIL import Image
@@ -13,7 +12,6 @@ import simpleaudio as sa
 from scipy.io import wavfile
 from librosa import display
 import matplotlib.pyplot as plt
-import torch
 import os
 # Load the example clip
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -26,21 +24,23 @@ def add_noise(audio, params={}, method='none'):
     if (method=='gaussian'):
         X_noise = add_gaussian_noise(X, params)
     elif(method=='min_suppression'):
+        print("Min suppression")
         X_noise = minimum_suppression(X, params)
     elif(method=='superimpose'):
         X_noise = superimpose(X, params)
     elif(method=='lengthen'):
-        X_noise = lengthen(audio)
-        return X_noise.astype(np.int16)
+        X_noise = lengthen(audio).astype(np.int16)
+        # sa.play_buffer(X_noise, 1, 2, 16000).wait_done()
+        return X_noise
     elif(method=='cutout'):
         X_noise = add_cutout_noise(X, params)
     elif(method=='real_world_noise'):
-        X_noise = add_real_world_noise(audio, params)
-        return X_noise.astype(np.int16)
+        X_noise = add_real_world_noise(X, params)
     else:
         X_noise = X
     # #convert audio back into signal
     Xn = librosa.istft(X_noise)
+    # sa.play_buffer(Xn.astype(np.int16), 1, 2, 16000).wait_done()
     #return the noisy audio signal as a short int for deepspeech
     return Xn.astype(np.int16)
 
@@ -104,34 +104,44 @@ def superimpose(X, params):
         SINObj.load_noise(noise_dir, noise_mag)
     return SINObj.add_noise(X)
 
-def lengthen(X):
-    X_long = np.zeros((X.shape[0], X.shape[1]*2), dtype=complex)
-    X_long[:,0:X_long.shape[1]:2] = X.copy()
-    X_long[:,1:X_long.shape[1]-1:2] = (X_long[:,0:X_long.shape[1]-2:2].copy() + X_long[:,2:X_long.shape[1]:2].copy()) / 2
-    X_long[:,-1] = X_long[:,-2].copy()
-    return X_long
+def lengthen(audio):
+
+    audio_transforms = audiomentations.Compose([
+        TimeStretch(min_rate=0.5, max_rate=0.5, leave_length_unchanged=False, p=1)
+    ])
+
+    y = audio_transforms(samples=audio, sample_rate=8000)
+    # play the augmented audio signal
+
+    return y
 
 def add_real_world_noise(audio, params):
     # params is the SNR, range from 90 to 110.
     # read_dir = 'realworld/output{}.wav'.format()
     soundfiles = glob.glob("realworld/*.wav")
     samplerate, data = wavfile.read(random.choice(soundfiles))
-    noise = data[:len(audio),0]
-    if(len(data) < len(audio)): #in the case that the noise audio is shorter than the actual audio, just replay
-        noise = np.zeros((len(audio)))
+    noise_spec = librosa.stft(data[:,0])
+    if(noise_spec.shape[1] > audio.shape[1]):
+        noise_spec = noise_spec[:,:audio.shape[1]]
+    noise = np.zeros(audio.shape, dtype=complex)
+    noise[:,:noise_spec.shape[1]] = noise_spec[:audio.shape[0],:]
+    if(len(noise_spec) < len(audio)): #in the case that the noise audio is shorter than the actual audio, just replay
         for i in range(0, len(data), len(audio)):
-            noise[i:i+len(data)] = data[:,0].copy()
-    RMS_noise = np.mean(audio**2)/math.sqrt(10**(params['SNR']/10))
-    a = np.divide(RMS_noise,np.mean(noise**2))
+            noise[i:i+len(data),:] = noise_spec.copy()
+        mod = len(audio) % len(data)
+        noise[-mod:,:] = noise_spec[:mod,:].copy()
+    # #this is E[N^2], so we want the squared mean of the noise to match this value
+    RMS_noise = np.mean(audio**2)/params['SNR']
+    a = np.sqrt(np.divide(RMS_noise,np.mean(noise**2)))
+    result_noise = np.multiply(a,noise)
     result = audio+np.multiply(a,noise)
 
-    # sa.play_buffer(noised_audio.astype(np.int16), 1, 2, 16000)
     return result
 
 def add_cutout_noise(X, params):
     h, w = X.shape[0], X.shape[1]
     mask = np.ones((h, w), np.int16)
-    for n in range(params):
+    for n in range(params["cutout"]):
         y = np.random.randint(h-10)
         x = np.random.randint(w-10)
 
@@ -157,6 +167,5 @@ def apply_audio_transforms(audio):
     y = audio_transforms(samples=audio, sample_rate=16000)
 
     # play the augmented audio signal
-    sa.play_buffer(audio.astype(np.int16), 1, 2, 16000)
 
     return y
